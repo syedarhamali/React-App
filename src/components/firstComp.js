43a967ef5df86d02163051365b7d139e4b3e14b6
Arham~ <53353729+syedarhamali@users.noreply.github.com>
2022-10-30 14:39:41 +0500

    my-first-class and function component

---
+++ src/components/firstComp.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+ render () {
+   const cityArray = ['Karachi', 'Lahore', 'Peshawar', 'Quetta']
+
+   const shortListedCities = cityArray.filter(city => city.length > 6)
+   return (
+     <ul>
+       {
+         shortListedCities.map(city => <li key={city}> {city} </li>)
+         
+       }
+     </ul>
+   )
+ }
+}
+
+function App1 () {
+ return (<MyComponent />)
+}
+
+export default App1;
